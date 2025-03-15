1ea654ab1ef8bfd6bcaaeb277610649ac8b081ab
asmitatemkar <asmitasunil22@gmail.com>
2025-03-15 12:13:23 +0530

    home page ui

--- web/src/layout/Navbar.jsx
+++ web/src/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import { LogInIcon } from "lucide-react";
+import { LogInIcon, ShoppingCartIcon } from "lucide-react";
 import { Link } from "react-router";
 
 const Navbar = () => {
@@ -6,13 +6,23 @@ const Navbar = () => {
     <nav className="flex justify-between items-center px-10 py-4 shadow-md">
       <div>
         <Link to="/" className="text-2xl font-bold tracking-tighter">
-          FlyMart
+          <div className="flex space-x-2 items-center">
+            <img src="./icons/mart.png" className="w-12 h-12" />
+            <span>FlyMart</span>
+          </div>
         </Link>
       </div>
       <div className="flex space-x-2">
         <Link
+          to="/cart"
+          className="flex items-center bg-blue-500 text-white px-4 py-1.5 rounded-md hover:cursor-pointer active:bg-gray-800 hover:shadow-lg text-sm"
+        >
+          <ShoppingCartIcon className="h-4 w-4 mr-2" />
+          <span>Cart</span>
+        </Link>
+        <Link
           to="/login"
-          className="flex items-center bg-black text-white px-4 py-1.5 rounded-full hover:cursor-pointer active:bg-gray-800 hover:shadow-lg text-sm"
+          className="flex items-center bg-blue-500 text-white px-4 py-1.5 rounded-full hover:cursor-pointer active:bg-gray-800 hover:shadow-lg text-sm"
         >
           <LogInIcon className="h-4 w-4 mr-2" />
           <span>Login</span>
--- web/src/pages/HomePage.jsx
+++ web/src/pages/HomePage.jsx
@@ -1,5 +1,27 @@
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="flex flex-col space-y-5 px-10 py-5 bg-gray-200">
+      <div className="flex space-x-5 bg-white p-5 rounded-lg shadow-lg">
+        <div className="bg-blue-500 w-20 h-20 flex justify-center items-center text-white rounded-lg hover:cursor-pointer hover:-translate-y-1 hover:shadow-lg ease-in-out transition-all duration-300">
+          Hello
+        </div>
+      </div>
+      <div className="flex space-x-5">
+        <div className="flex flex-col w-1/2 space-x-5 bg-white p-5 rounded-lg shadow-lg">
+          <h1 className="text-xl font-bold tracking-tighter mb-4">
+            Category 1
+          </h1>
+          <div className="flex flex-col space-y-5">Hello</div>
+        </div>
+        <div className="flex flex-col w-1/2 space-x-5 bg-white p-5 rounded-lg shadow-lg">
+          <h1 className="text-xl font-bold tracking-tighter mb-4">
+            Category 2
+          </h1>
+          <div className="flex flex-col space-y-5">Hello</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
